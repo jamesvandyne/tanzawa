694d0b98bc4dc6681919f19e2d166824d53878cc
James Van Dyne <james@sugoisoft.com>
2021-01-02 07:12:38 +0900

    add default colors and rework base design of posting screens

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -2,6 +2,47 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      'white': '#ffffff',
+      'transparent': 'transparent',
+      'current': 'currentColor',
+      'bianca': {
+        '50': '#fffffe',
+        '100': '#fffefd',
+        '200': '#fefdfb',
+        '300': '#fdfcf9',
+        '400': '#fcf9f4',
+        '500': '#fbf7ef',
+        '600': '#e2ded7',
+        '700': '#bcb9b3',
+        '800': '#97948f',
+        '900': '#7b7975'
+      },
+      'negroni': {
+        '50': '#fffdfc',
+        '100': '#fefbf8',
+        '200': '#fdf5ee',
+        '300': '#fbefe3',
+        '400': '#f8e2cf',
+        '500': '#f5d6ba',
+        '600': '#ddc1a7',
+        '700': '#b8a18c',
+        '800': '#938070',
+        '900': '#78695b'
+      },
+      'malachite': {
+        '50': '#f4fef7',
+        '100': '#e9fdef',
+        '200': '#c8fad6',
+        '300': '#a7f6bd',
+        '400': '#64f08c',
+        '500': '#22e95a',
+        '600': '#1fd251',
+        '700': '#1aaf44',
+        '800': '#148c36',
+        '900': '#11722c'
+      },
+    },
     extend: {},
   },
   variants: {
