6f237c731c1f893d1a6261f7f1daaafed486f78a
James Van Dyne <james@sugoisoft.com>
2021-01-02 05:51:36 +0900

    add tailwindcss proper

--- apps/core/settings.py
+++ apps/core/settings.py
@@ -124,7 +124,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "../static/",
+]
+STATIC_ROOT = BASE_DIR / "../staticfiles/"
 
+# import pdb; pdb.set_trace()
 
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
