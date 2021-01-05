407e15ae30461d8f95e4a07b98ac6f68308d11a8
James Van Dyne <james@sugoisoft.com>
2021-01-06 06:51:01 +0900

    refs 1 fix db engine settings

--- apps/core/settings.py
+++ apps/core/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = "core.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
+        "ENGINE": "django.contrib.gis.db.backends.spatialite",
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
