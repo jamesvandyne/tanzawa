b48e48a3ee302d46aedb3559c720be0caf7ac8ae
James Van Dyne <james@sugoisoft.com>
2021-03-14 04:44:23 +0900

    refs #42 refactor public entry templates to share a footer. highlight selected streams in post detail. add home link

--- apps/public/views.py
+++ apps/public/views.py
@@ -9,7 +9,7 @@ from streams.models import MStream
 
 def home(request):
     context = {
-        "entries": TEntry.objects.select_related("t_post", "t_post__p_author")
+        "entries": TEntry.objects.select_related("t_post", "t_post__p_author", "t_location", "t_bookmark", "t_reply")
         .filter(t_post__m_post_status__key=MPostStatuses.published)
         .annotate(
             interaction_count=Count(
@@ -17,7 +17,7 @@ def home(request):
                 filter=Q(t_post__ref_t_webmention__approval_status=True),
             )
         ),
-        "selected": "status",
+        "selected": ["home"],
         "streams": MStream.objects.visible(request.user),
     }
     return render(request, "public/index.html", context=context)
@@ -25,7 +25,7 @@ def home(request):
 
 def status_detail(request, uuid):
     t_post: TPost = get_object_or_404(
-        TPost.objects.prefetch_related("ref_t_entry", "ref_t_entry__t_reply").filter(
+        TPost.objects.prefetch_related("ref_t_entry", "ref_t_entry__t_reply", "ref_t_entry__t_location", "ref_t_entry__t_bookmark").filter(
             m_post_status__key=MPostStatuses.published
         ),
         uuid=uuid,
@@ -40,6 +40,7 @@ def status_detail(request, uuid):
         "webmentions_count": webmentions.count(),
         "t_entry": t_entry,
         "now": now(),
+        "selected": t_post.streams.values_list('slug', flat=True),
         "title": t_entry.p_name if t_entry.p_name else t_entry.p_summary[:140],
         "streams": MStream.objects.visible(request.user),
         "public": True,
@@ -73,7 +74,7 @@ def stream(request, stream_slug: str):
             )
         ),
         "stream": stream,
-        "selected": stream.slug,
+        "selected": [stream.slug],
         "streams": MStream.objects.visible(request.user),
     }
     return render(request, "public/index.html", context=context)
