885fc7301ccc774b804728e2e48cb1368e5d455d
James Van Dyne <james@sugoisoft.com>
2021-04-20 05:17:03 +0900

    hotfix: allow feed url to work with/without a /

--- apps/feeds/urls.py
+++ apps/feeds/urls.py
@@ -5,6 +5,6 @@ from . import views
 app_name = "feeds"
 
 urlpatterns = [
-    path("feed", views.AllEntriesFeed(), name="feed"),
-    path("<slug:stream_slug>/feed", views.StreamFeed(), name="stream_feed"),
+    path("feed/", views.AllEntriesFeed(), name="feed"),
+    path("<slug:stream_slug>/feed/", views.StreamFeed(), name="stream_feed"),
 ]
