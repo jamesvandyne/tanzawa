052df12034159b5c75efaa4e88b5de560eea54ef
James Van Dyne <james@sugoisoft.com>
2021-05-14 05:57:45 +0900

    add snippet field

--- apps/settings/models.py
+++ apps/settings/models.py
@@ -7,6 +7,12 @@ class MSiteSettings(TimestampModel):
     title = models.CharField(max_length=128, default="Tanzawa", blank=True)
     subtitle = models.CharField(max_length=128, default="", blank=True)
 
+    footer_snippet = models.TextField(
+        blank=True,
+        default='<span class="help-text">Powered by <a href="http://tanzawa.blog"><span class="mr-1">🏔</span>Tanzawa</a></span>',
+        help_text="Add an html snippet that will appear in the footer of your site.",
+    )
+
     class Meta:
         verbose_name = "Site Settings"
         verbose_name_plural = "Site Settings"
