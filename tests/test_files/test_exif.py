36f933de0f1680bd7ddc7bfd53c593aab2b8be0e
James Van Dyne <james@sugoisoft.com>
2021-05-29 15:19:46 +0900

    add test for get location

--- tests/test_files/test_exif.py
+++ tests/test_files/test_exif.py
@@ -3,17 +3,18 @@ import pytest
 from exif import Image
 
 
+@pytest.fixture
+def image():
+    with open("tests/fixtures/img_gps.jpeg", "rb") as f:
+        return io.BytesIO(f.read())
+
+
 class TestScrubExif:
     @pytest.fixture
     def target(self):
         from files.exif import scrub_exif
 
         return scrub_exif
-
-    @pytest.fixture
-    def image(self):
-        with open("tests/fixtures/img_gps.jpeg", "rb") as f:
-            return io.BytesIO(f.read())
 
     def test_removes_location(self, target, image):
         """
@@ -29,3 +30,21 @@ class TestScrubExif:
         scrubbed_image = Image(scrubbed_bytes)
         assert scrubbed_image.get("gps_latitude") is None
         assert scrubbed_image.get("gps_longitude") is None
+
+
+class TestExtractsLocation:
+    @pytest.fixture
+    def target(self):
+        from files.exif import get_location
+
+        return get_location
+
+    @pytest.fixture
+    def exif_dict(self, image):
+        img_dict = Image(image.read())
+        return img_dict
+
+    def test_extracts_point(self, target, exif_dict):
+        result = target(exif_dict)
+        assert result.x == 139.4856722222222  # lon
+        assert result.y == 35.31948333333334  # lat
