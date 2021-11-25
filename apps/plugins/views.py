eb6fc17ceccbceed9b925b74b52769185e9192de
James Van Dyne <james@sugoisoft.com>
2021-11-26 06:21:08 +0900

    Make Plugin Screens Login Required and Add Titles / Nav to Plugin List

--- apps/plugins/views.py
+++ apps/plugins/views.py
@@ -1,8 +1,10 @@
 from django.contrib import messages
 from django.shortcuts import redirect, render
 from plugins.core import plugin_pool
+from django.contrib.auth import decorators as auth_decorators
 
 
+@auth_decorators.login_required
 def plugin_list(request):
 
     return render(
@@ -10,10 +12,13 @@ def plugin_list(request):
         "plugins/plugin_list.html",
         {
             "available_plugins": plugin_pool.get_all_plugins(),
+            "nav": "plugins",
+            "page_title": "Plugins"
         },
     )
 
 
+@auth_decorators.login_required
 def enable_plugin(request, identifier: str):
     plugin = plugin_pool.get_plugin(identifier)
     if not plugin:
@@ -24,6 +29,7 @@ def enable_plugin(request, identifier: str):
     return redirect("plugins:plugin_list")
 
 
+@auth_decorators.login_required
 def disable_plugin(request, identifier: str):
     plugin = plugin_pool.get_plugin(identifier)
     if not plugin:
