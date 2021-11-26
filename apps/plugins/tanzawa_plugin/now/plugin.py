7d777df163db5972617170f78f11994b7b6b3603
James Van Dyne <james@sugoisoft.com>
2021-11-27 06:39:11 +0900

    Update Now Icon

--- apps/plugins/tanzawa_plugin/now/plugin.py
+++ apps/plugins/tanzawa_plugin/now/plugin.py
@@ -25,7 +25,7 @@ class NowPlugin(plugin.Plugin):
 
     def public_top_nav_icon(self) -> str:
         """Return an emoji that will be displayed next to the top nav item."""
-        return "🔛"
+        return "👉"
 
     def public_top_nav_content(self) -> str:
         """Return html to be output on the page after the top nav icon"""
