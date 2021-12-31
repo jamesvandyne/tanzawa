0e6ded8ed8396cbad27ecddf6de5995f0cf1e589
James Van Dyne <james@sugoisoft.com>
2022-01-01 08:14:38 +0900

    Generate webpack configuration for each theme

--- front/webpack.config.js
+++ front/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const fs = require('fs');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const adminConfig = {
@@ -52,6 +53,55 @@ const tailwindConfig = {
     contentBase: path.resolve(__dirname, "dist"),
     open: true,
   },
+};
+
+
+/* Build a webpack config file each theme */
+const themes = fs.readdirSync('./themes/');
+const themeConfigs = [];
+const outputPathParts = ["../static/themes/"];
+
+for (const themeDirectory of themes) {
+  // themeDirectory: /full/path/to/code/front/platinum
+  let themeName = path.basename(themeDirectory); // platinum
+  let outputPath = path.resolve(...outputPathParts, themeName);  // ../static/themes/platinum/
+  let themeDirectoryPathParts = [__dirname, "themes", themeName];  // front/themes/platinum
+  let entryPoint = path.resolve(...themeDirectoryPathParts,  "index.js"); // front/themes/platinum/index.js
+  let themeConfig = {
+    entry: entryPoint,  // front/themes/platinum/index.js
+    output: {
+      path: outputPath  // ../static/themes/platinum/
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+        filename: 'style.css'
+      })
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            { loader: "css-loader", options: { importLoaders: 1 } },
+            { loader: "postcss-loader",
+              options: {
+                postcssOptions: {
+                  plugins: () => [
+                      require('postcss-import'),
+                      require("tailwindcss")( {config:`./themes/${themeName}/tailwind.config.js` }),
+                      require("autoprefixer")
+                  ]
+                }
+              }
+            },
+          ],
+        },
+      ],
+    }
+  };
+  themeConfigs.push(themeConfig);
 }
 
-module.exports = [adminConfig, publicConfig, tailwindConfig];
+
+module.exports = [adminConfig, publicConfig, tailwindConfig, ...themeConfigs];
