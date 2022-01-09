8123d3e7fab13eede7dff00d9456810cf8b930df
James Van Dyne <james@sugoisoft.com>
2022-01-10 07:37:18 +0900

    Allow theme to be set via Django admin

--- apps/settings/admin.py
+++ apps/settings/admin.py
@@ -1,5 +1,24 @@
+from django import forms
+from django.conf import settings
 from django.contrib import admin
 
 from .models import MSiteSettings
 
-admin.site.register(MSiteSettings)
+THEME_CHOICES = [
+    ["", "Tanzawa"],
+] + [(theme, theme.title()) for theme in settings.THEMES]
+
+
+class MSiteSettingsAdminForm(forms.ModelForm):
+    class Meta:
+        model = MSiteSettings
+        fields = "__all__"
+
+    theme = forms.ChoiceField(choices=THEME_CHOICES, required=False)
+
+
+class MSiteSettingsAdmin(admin.ModelAdmin):
+    form = MSiteSettingsAdminForm
+
+
+admin.site.register(MSiteSettings, MSiteSettingsAdmin)
