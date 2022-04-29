94447e041716f9b4c51287955552e199e29ce00e
James Van Dyne <james@sugoisoft.com>
2022-04-30 05:21:25 +0900

    Add newlines between content and plugin content in feeds

--- apps/application/feeds/content.py
+++ apps/application/feeds/content.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from data.plugins.pool import plugin_pool
 from data.post import models as post_models
 from domain.feeds import queries
@@ -7,10 +9,17 @@ def get_encoded_content(*, post: post_models.TPost) -> str:
     """
     Get the html encoded for a given post wrapped with plugin content before/after the post.
     """
-    before_content = ""
-    after_content = ""
+    before_content: List[str] = []
+    after_content: List[str] = []
+    br_tag = "<br/>"
     content = queries.get_encoded_content(post)
     for feed_plugin in plugin_pool.feed_plugins():
-        before_content += feed_plugin.feed_before_content(post=post)
-        after_content += feed_plugin.feed_after_content(post=post)
-    return f"{before_content}{content}{after_content}"
+        before_content.append(feed_plugin.feed_before_content(post=post))
+        after_content.append(feed_plugin.feed_after_content(post=post))
+
+    if before_content:
+        content = f"{br_tag.join(before_content)}{br_tag}{content}"
+    if after_content:
+        content = f"{content}{br_tag}{br_tag.join(after_content)}"
+
+    return content
