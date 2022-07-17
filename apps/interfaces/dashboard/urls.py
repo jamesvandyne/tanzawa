2ea37e9788b6812d71d1dc5d5804a10f5751f6b9
James Van Dyne <james@sugoisoft.com>
2022-07-18 07:54:33 +0900

    Remove extra /a/ from plugin admin urls

--- apps/interfaces/dashboard/urls.py
+++ apps/interfaces/dashboard/urls.py
@@ -17,7 +17,7 @@ plugin_urls = [path("", include(plugin_urls)) for plugin_urls in pool.plugin_poo
 urlpatterns.extend(plugin_urls)
 
 plugin_admin_urls = [
-    path(f"a/plugins/{text.slugify(plugin.name)}/", include(plugin.admin_urls))
+    path(f"plugins/{text.slugify(plugin.name)}/", include(plugin.admin_urls))
     for plugin in pool.plugin_pool.enabled_plugins()
     if plugin.admin_urls
 ]
