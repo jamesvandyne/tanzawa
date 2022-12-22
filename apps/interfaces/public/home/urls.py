88a01c6b7b68383a4563be93b9728f04670c5d37
James Van Dyne <james@vandyne.co>
2022-12-22 17:08:37 +0900

    Add blog to top navigation

--- apps/interfaces/public/home/urls.py
+++ apps/interfaces/public/home/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("blog/", views.BlogListView.as_view(), name="blog"),
     path("", views.HomeView.as_view(), name="home"),
 ]
