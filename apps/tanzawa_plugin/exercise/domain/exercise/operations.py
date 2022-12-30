b83e927e69b9737b4f292b0c9593a6fa3c80a530
James Van Dyne <james@vandyne.co>
2022-12-30 18:52:18 +0900

    Record map when it exists on an activity

--- apps/tanzawa_plugin/exercise/domain/exercise/operations.py
+++ apps/tanzawa_plugin/exercise/domain/exercise/operations.py
@@ -56,6 +56,48 @@ def record_activity(activity: Activity) -> models.Activity:
         average_heartrate=activity.average_heartrate,
         max_heartrate=activity.max_heartrate,
     )
-    # TODO: Record Map record if it exists
+    if activity.activity_map:
+        models.Map.objects.create(
+            activity=new_activity,
+            vendor_id=activity.activity_map.vendor_id,
+            summary_polyline=activity.activity_map.summary_polyline,
+        )
 
     return new_activity
+
+
+@transaction.atomic
+def update_activity(instance: models.Activity, activity: Activity) -> None:
+    instance.update(
+        name=activity.name,
+        vendor_id=activity.vendor_id,
+        upload_id=activity.upload_id,
+        distance=activity.distance,
+        moving_time=activity.moving_time,
+        elapsed_time=activity.elapsed_time,
+        total_elevation_gain=activity.total_elevation_gain,
+        elevation_high=activity.elevation_high,
+        elevation_low=activity.elevation_low,
+        activity_type=activity.activity_type,
+        started_at=activity.started_at,
+        start_point=activity.start_point,
+        end_point=activity.end_point,
+        average_speed=activity.average_speed,
+        max_speed=activity.max_speed,
+        average_heartrate=activity.average_heartrate,
+        max_heartrate=activity.max_heartrate,
+    )
+    if activity.activity_map:
+        try:
+            activity_map = instance.map
+        except models.Map.DoesNotExist:
+            models.Map.objects.create(
+                activity=instance,
+                vendor_id=activity.activity_map.vendor_id,
+                summary_polyline=activity.activity_map.summary_polyline,
+            )
+        else:
+            activity_map.update(
+                vendor_id=activity.activity_map.vendor_id,
+                summary_polyline=activity.activity_map.summary_polyline,
+            )
