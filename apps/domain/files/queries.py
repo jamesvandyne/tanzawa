efb131f51dc6b05ccb68327270153e4fa8e28da8
James Van Dyne <james@vandyne.co>
2023-05-03 08:19:49 +0900

    Return a fix size for non-image attachments

--- apps/domain/files/queries.py
+++ apps/domain/files/queries.py
@@ -1,4 +1,7 @@
+from dataclasses import dataclass
+
 from django.db.models import QuerySet
+from PIL import Image, UnidentifiedImageError
 
 from core.constants import Visibility
 from data.files import models as file_models
@@ -24,3 +27,21 @@ def get_representative_image(post: post_models.TPost) -> file_models.TFile | Non
     Return the first image in a post that's used as the representative image.
     """
     return post.files.filter(mime_type__startswith="image").first()
+
+
+@dataclass(frozen=True)
+class Size:
+    width: int
+    height: int
+
+
+def get_size_for_file(t_file: file_models.TFile) -> Size:
+    """
+    Return the size for a given file.
+    """
+    try:
+        with Image.open(t_file.file) as image:
+            return Size(width=image.width, height=image.height)
+    except UnidentifiedImageError:
+        # Return a fixed size if case we upload a PDF or some non-Image file.
+        return Size(width=600, height=600)
