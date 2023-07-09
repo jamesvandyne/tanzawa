528454eb38aecab2eff6821f39100919738dd47b
James Van Dyne <james@vandyne.co>
2023-07-09 13:06:34 +0900

    Add queries for getting related post status/kind db records from constants
    Post status and kind as db records should eventually be factored out in favor of just using the constant everywhere.

--- apps/domain/post/queries.py
+++ apps/domain/post/queries.py
@@ -1,5 +1,14 @@
+from data.indieweb.constants import MPostKinds, MPostStatuses
 from data.post import models
 
 
 def get_t_post_by_uuid(uuid: str) -> models.TPost:
     return models.TPost.objects.get(uuid=uuid)
+
+
+def get_post_status(status: MPostStatuses) -> models.MPostStatus:
+    return models.MPostStatus.objects.get(key=status)
+
+
+def get_post_kind(kind: MPostKinds) -> models.MPostKind:
+    return models.MPostKind.objects.get(key=kind)
