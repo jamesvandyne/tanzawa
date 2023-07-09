0c19fc989a49ab3ad7d726fe7ed4073cda761647
James Van Dyne <james@vandyne.co>
2023-07-09 13:08:20 +0900

    Add query for fetching strava activity detail urls

--- apps/tanzawa_plugin/exercise/data/strava/constants.py
+++ apps/tanzawa_plugin/exercise/data/strava/constants.py
@@ -1,3 +1,4 @@
 STRAVA_OAUTH_AUTHORIZE_URL = "https://www.strava.com/oauth/authorize"
 STRAVA_OAUTH_TOKEN_URL = "https://www.strava.com/oauth/token"
 STRAVA_ACTIVITIES_ENDPOINT = "https://www.strava.com/api/v3/athlete/activities"
+STRAVA_ACTIVITY_URL = "https://www.strava.com/activities/{id}"
--- apps/tanzawa_plugin/exercise/domain/strava/queries.py
+++ apps/tanzawa_plugin/exercise/domain/strava/queries.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.contrib.auth import models as auth_models
 
-from tanzawa_plugin.exercise.data.strava import models
+from tanzawa_plugin.exercise.data.strava import constants, models
 
 
 def is_strava_environment_setup() -> bool:
@@ -10,3 +10,7 @@ def is_strava_environment_setup() -> bool:
 
 def is_user_connected_to_strava(user: auth_models.User) -> bool:
     return models.Athlete.objects.filter(user=user).exists()
+
+
+def get_activity_url(strava_activity_id: str) -> str:
+    return constants.STRAVA_ACTIVITY_URL.format(id=strava_activity_id)
