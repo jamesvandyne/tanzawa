b8f56c8e49800e7949aac36d0d6b2e8bd4f3547e
James Van Dyne <james@vandyne.co>
2023-11-19 05:46:15 +0900

    Move checking rss rendering to templates

--- apps/domain/feeds/queries.py
+++ apps/domain/feeds/queries.py
@@ -10,14 +10,15 @@ def get_encoded_content(post: post_models.TPost) -> str:
     """
     entry = post.ref_t_entry
     e_content = entry.e_content
-    context = {"e_content": e_content}
+    context = {"e_content": e_content, "post": post, "entry": entry}
     if entry.is_reply:
         e_content = f"<blockquote>{entry.t_reply.quote}</blockquote>{e_content}"
     elif entry.is_bookmark:
         context["t_bookmark"] = entry.t_bookmark
         e_content = render_to_string("public/feeds/bookmark.html", context=context)
     elif entry.is_checkin:
-        e_content = f"{post.post_title}<br/>{e_content}"
+        context["t_checkin"] = entry.t_checkin
+        e_content = render_to_string("public/feeds/checkin.html", context=context)
     try:
         e_content = f"{e_content}<br/>Location: {entry.t_location.summary}"
     except entry_models.TLocation.DoesNotExist:
