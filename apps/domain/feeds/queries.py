718f1e79f7030aa8ebb075cc095795d5aa3713ab
James Van Dyne <james@vandyne.co>
2023-11-19 06:02:00 +0900

    Moves reply rss content to templates

--- apps/domain/feeds/queries.py
+++ apps/domain/feeds/queries.py
@@ -12,7 +12,8 @@ def get_encoded_content(post: post_models.TPost) -> str:
     e_content = entry.e_content
     context = {"e_content": e_content, "post": post, "entry": entry}
     if entry.is_reply:
-        e_content = f"<blockquote>{entry.t_reply.quote}</blockquote>{e_content}"
+        context["t_reply"] = entry.t_reply
+        e_content = render_to_string("public/feeds/reply.html", context=context)
     elif entry.is_bookmark:
         context["t_bookmark"] = entry.t_bookmark
         e_content = render_to_string("public/feeds/bookmark.html", context=context)
