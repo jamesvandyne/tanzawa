3f641541a8a9e7d19e0ebaae9a010a5de48930ac
James Van Dyne <james@vandyne.co>
2023-11-20 17:11:02 +0900

    Add template tag for getting webmention emojis

--- apps/interfaces/common/templatetags/indieweb.py
+++ apps/interfaces/common/templatetags/indieweb.py
@@ -1,5 +1,7 @@
 from django import template
 
+from domain.indieweb import queries
+
 register = template.Library()
 
 
@@ -14,3 +16,9 @@ class RenderRelme(template.Node):
 @register.tag(name="render_relme")
 def do_render_relme(parser, token):
     return RenderRelme()
+
+
+@register.filter(name="webmention_emojis")
+def get_webmention_emojis(comment_types: list[str]) -> str:
+    emojis = [queries.emoji_for_kind(kind) for kind in comment_types]
+    return "".join([emoji for emoji in emojis if emoji])
