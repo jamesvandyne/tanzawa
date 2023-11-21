3f9ce63d83578ad80638910dc42c975bf04f93b4
James Van Dyne <james@vandyne.co>
2023-11-22 07:30:23 +0900

    Render activity details in exercise plugin

--- apps/tanzawa_plugin/exercise/plugin.py
+++ apps/tanzawa_plugin/exercise/plugin.py
@@ -1,11 +1,13 @@
-from typing import TYPE_CHECKING, Type
+from typing import TYPE_CHECKING, Optional, Type
 
 from django import http, template
 from django.template import loader
 
 from data.plugins import plugin, pool
+from data.plugins.plugin import RequestContentType
 
 from .data import exercise_models
+from .interfaces.public import serializers
 
 if TYPE_CHECKING:
     from data.post import models as post_models
@@ -35,6 +37,10 @@ class ExercisePlugin(plugin.Plugin):
     def has_feed_hooks(self):
         return True
 
+    @property
+    def has_content_hooks(self):
+        return True
+
     def render_navigation(
         self,
         *,
@@ -54,6 +60,24 @@ class ExercisePlugin(plugin.Plugin):
             return ""
 
         template = loader.get_template("exercise/public/feeds/activity_detail.html")
+        try:
+            activity = exercise_models.Activity.objects.get(entry_id=post.ref_t_entry.id)
+        except Exception:
+            return ""
+        else:
+            activity_detail = serializers.Activity(activity).data
+            return template.render(context={"activity": activity_detail}, request=request)
+
+    def render_after_content(
+        self,
+        request: http.HttpRequest,
+        request_content_type: RequestContentType = RequestContentType.HTML,
+        post: Optional["post_models.TPost"] = None,
+    ) -> str:
+        if post is None:
+            return ""
+
+        template = loader.get_template("exercise/public/fragments/activity_detail.html")
         try:
             activity = exercise_models.Activity.objects.get(entry_id=post.ref_t_entry.id)
         except Exception:
