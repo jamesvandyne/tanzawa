ad2726c777254a31ad7c78b617225a17589c160c
James Van Dyne <james@vandyne.co>
2024-12-07 09:07:33 +0900

    Add bluesky bridgy publish url

--- apps/data/entry/constants.py
+++ apps/data/entry/constants.py
@@ -3,3 +3,4 @@ from django.db.models import TextChoices
 
 class BridgySyndicationUrls(TextChoices):
     mastodon = "https://brid.gy/publish/mastodon"
+    bluesky = "https://brid.gy/publish/bluesky"
